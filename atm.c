cb68af0a207c039b2d1614a66de4fe65daaeba90
vaishnavi anabathini <247r1a66k5@cmrtc.ac.in>
2026-03-11 19:11:46 +0530

    Create atm.c

---
+++ atm.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int pin = 1234;       // Correct ATM PIN
+    int enteredPin;
+    int attempts = 0;
+
+    while(attempts < 3) {
+        printf("Enter your ATM PIN: ");
+        scanf("%d", &enteredPin);
+
+        if(enteredPin == pin) {
+            printf("PIN verified successfully!\n");
+            printf("Access granted.\n");
+            return 0;
+        } 
+        else {
+            printf("Incorrect PIN. Try again.\n");
+            attempts++;
+        }
+    }
+
+    printf("Too many incorrect attempts. Your card is blocked.\n");
+
+    return 0;
+}
